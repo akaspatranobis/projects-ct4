f217fb62826e7f0cf9199cee78f2d121a11b217d
Akas Patranobis <akaspatranobis@yahoo.com>
2025-02-01 16:37:13 +0530

    up

--- live-classes/terraform/conditions/main.tf
+++ live-classes/terraform/conditions/main.tf
@@ -1,6 +1,18 @@
+# ? and :
+# condition ? true_value : false_value
+
+
 resource "aws_instance" "test" {
+    count=2
     ami = "ami-0533f2ba8a1995cf9"
-    instance_type = var.instance_type == "t2*" ? "t3.micro" :  var.instance_type
+    instance_type = can(regex("^t2", var.instance_type)) ? "t3.micro" : var.instance_type
+    tags = {
+        Name = "Server - ${count.index}"
+  }
 }
 
 variable "instance_type" {}
+
+# variable "create_instance" {}
+
+#make this ami dynamic with the data block
